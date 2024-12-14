51fb498f2bee7afc5f0d86682d6b7fbc5337bb72
RajveerAgrawal <jbcnrajveer@gmail.com>
2024-12-14 16:11:28 +0530

    class 2 java

--- class 1/project.java
+++ class 1/project.java
@@ -1,6 +1,8 @@
 public class project { 
     public static void main (String[] args)
     {
+        int a = 5, b = 2; 
+        System.out.println(a+b);
         System.out.println("welcome buddy");
     }
 }
---
+++ class 2/activity.java
@@ -0,0 +1,26 @@
+class activity
+{ 
+    public static void main (String[] args)
+
+{
+    // int a=5, b=6;
+    // System.out.println(a+b);
+    // System.out.println(a-b);
+    // System.out.println(a/b);
+    // System.out.println(a*b);
+    // int x=9, y=12;
+    // System.out.println(x++);
+    // System.out.println(x);
+    // System.out.println(++y);
+    // String ans=(x>y)? "A is bigger":"B is bigger";
+    // System.out.println(ans);
+    System.out.println("My Maggie Recipe");
+    Maggie();
+}
+
+public static void  Maggie(){
+    System.out.println("1.Boil water in pan");
+    System.out.println("2.Now add your special maggie recipe afer this");
+}
+
+}
