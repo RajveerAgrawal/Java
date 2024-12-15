c2d5132c821f0bd534930da8346c9b430133bf76
RajveerAgrawal <jbcnrajveer@gmail.com>
2024-12-15 17:48:45 +0530

    acp 2

---
+++ acp 2/activity.java
@@ -0,0 +1,10 @@
+class activity
+{ 
+    public static void main (String[] args)
+
+{
+    int a=8, b=6;
+   int z = a+b; 
+    System.out.println(z);
+}
+} 
---
+++ class 3/main.java
@@ -0,0 +1,17 @@
+class main {
+    public static void main(String[] args) {
+        int a =-2;
+        if (a==0)
+        {
+            System.out.println("A is 0");
+        }
+        else if (a<0)
+        {
+            System.out.println("A is negative");
+        }
+        else
+        {
+            System.out.println("A is positive");
+        }
+    }
+}
